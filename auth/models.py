851583d618dad1e25e6cb6dc7a69d1d9fc0de815
cjephuneh <calebjephuneh@gmail.com>
2023-10-19 15:27:15 +0300

    more

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class UserRegister(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    phone = models.CharField(max_length=10)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+class UserLogin(models.Model):
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    
+
+
+class ForgotPassword(models.Model):
+    email = models.EmailField()
+    otp = models.CharField(max_length=6)
+
+    
+    class Meta: 
+        db_table = 'user'
+
+    def __str__(self):
+        return self.name
