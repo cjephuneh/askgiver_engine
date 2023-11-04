45ae977e86895ce429899056f9de0538cdeecaf8
cjephuneh <calebjephuneh@gmail.com>
2023-11-04 20:16:31 +0300

    apps

--- csrinitiative/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import CsrInitiative
-
-class CsrInitiativeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CsrInitiative
-        fields = '__all__'
